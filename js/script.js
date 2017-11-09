fa8156c688fa0e9077dc0e5a097bc202810c41fc
Max Nichols <allenmaxnichols@gmail.com>
2017-11-08 21:19:36 -0600

    Added scss and Ajax
    included the data in the js/script.js file.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-	console.log("all good");
+	
+
+	var randomUserAPI = 'https://randomuser.me/api/?nat=us?results=2';
+	var randomUserOptions = {
+		dataType: 'json'
+	};
+	 function showData(data){
+	 		var userPhoto = '<img src="'+ data.results[0].picture.large +'">';
+	 		$('#employee-card').html(userPhoto);
+
+	 		console.log(data);
+	 }
+	$.getJSON(randomUserAPI, randomUserOptions, showData);
 });
