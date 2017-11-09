3b72cc2f7932348a90925bc4c846bdb4107bf9fe
Max Nichols <allenmaxnichols@gmail.com>
2017-11-08 22:47:39 -0600

    img data
    found path to image data.

--- js/script.js
+++ js/script.js
@@ -6,10 +6,17 @@ $(document).ready(function(){
 		dataType: 'json'
 	};//end AJAX
 	 function showData(data){
-	 		var userPhoto = '<img src="'+ data.results[0].picture.large +'">';
-	 		$('#employee-card').html(userPhoto);
+	 		
 
+	 		$.each(data.results, function(i, picture){
+	 			var image = data.results[i].picture;
+	 			console.log(image);
+	 			
+	 		});// end of the EACH
+	 		// $('#employee-card').html(userPhoto);
 	 		console.log(data);
+	 		
+	 		
 	 }
 	$.getJSON(randomUserAPI, randomUserOptions, showData);
 });
