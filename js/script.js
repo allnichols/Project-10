660fb7262dc71a802fb139dee525c684db217ec3
Max Nichols <allenmaxnichols@gmail.com>
2017-11-09 20:15:23 -0600

    Push data to particular arrays
    There is a bug with sass. Keeps saying there is an undefined mixin 'mq'!

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
 $(document).ready(function(){
 	
 	
+	const photos = [];
+	const names = [];
+	const number = [];
+	const emails = [];
+	const cities = [];
+	const residence = [];
+
 
 	var randomUserAPI = 'https://randomuser.me/api/?results=12&nat=us';
 	var randomUserOptions = {
 		dataType: 'json'
 	};//end AJAX
+
 	 function employeeData(data){
 	 		
 		let employeeDetails = data.results;
-		console.log(employeeDetails);
-		for(let i=0; i<employeeDetails.length; i++){
+		//console.log(employeeDetails);
+		for(let i=0; i< employeeDetails.length; i++){
 			let member = employeeDetails[i];
 			let image = member.picture.large;
 			let firstName = member.name.first;
@@ -23,11 +31,19 @@ $(document).ready(function(){
 			let state = member.location.state;
 			let zip = member.location.postcode;
 			let address = street +', ' + state + ' ' + zip;
-			console.log(address);
+			photos.push(image);
+			names.push(Name);
+			number.push(phone);
+			emails.push(email);
+			cities.push(city);
+			residence.push(address);
+			
+		
 		};
-	 		
-	 		
-	 		
+		
+			console.log(names, photos, number, emails, cities, residence);
 	 }
+
+
 	$.getJSON(randomUserAPI, randomUserOptions, employeeData);
 });
