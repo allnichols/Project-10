5062a1689af12c1d713a8e425b8fd1d85e6053fc
Max Nichols <allenmaxnichols@gmail.com>
2017-11-12 10:38:47 -0600

    Deleted sass and refactored js

--- js/script.js
+++ js/script.js
@@ -1,70 +1,34 @@
 $(document).ready(function(){
 	
-	const photos = [];
-	const names = [];
-	const number = [];
-	const emails = [];
-	const cities = [];
-	const residence = [];
 	
-	// const details = {
-	// 	image: photos,
-	// 	name: names,
-	// 	numbers: number,
-	// 	city:cities
-	// };
-	
-	//console.log(details);
-
 	var randomUserAPI = 'https://randomuser.me/api/?results=12&nat=us';
 	var randomUserOptions = {
 		dataType: 'json'
 	};//end AJAX
 
 	 function employeeData(data){
-	 		
+	 	//getting employee info
+		
 		let employeeDetails = data.results;
-		//console.log(employeeDetails);
+		let cardHTML = '<section>';
+		
 		for(let i=0; i< employeeDetails.length; i++){
 			let member = employeeDetails[i];
-			let image = member.picture.large;
-			let firstName = member.name.first;
-			let lastName = member.name.last;
-			let Name = firstName + ' ' + lastName;
-			let phone = member.phone;
-			let email = member.email;
-			let city = member.location.city;
-			let street = member.location.street;
-			let state = member.location.state;
-			let zip = member.location.postcode;
-			let address = street +', ' + state + ' ' + zip;
-			photos.push(image);
-			names.push(Name);
-			number.push(phone);
-			emails.push(email);
-			cities.push(city);
-			residence.push(address);
+			cardHTML += '<div class="member">';
+			cardHTML += '<img src="' + member.picture.large + '">';
+			cardHTML += '<h3>' + member.name.first + " " + member.name.last + '</h3>';
+			cardHTML += '<p>' + member.email + '</p>';
+			cardHTML += '<p>' + member.location.city + '</p>';
+			cardHTML += '</div>';
+			console.log(member);
+
 		};
+		cardHTML += '</section>';
+		document.getElementById("wrapper").innerHTML = cardHTML;
 
-		function test(){
-			var card = '<section>';
-		for(var i = 0; i < photos.length ; i++){
-		    card += '<div class="member">';
-			card += '<img src="' + photos[i] +'">';
-			card += '</div>';
-			console.log(photos[i]);
-		}
-			card += '</section>';
-		document.getElementById("wrapper").innerHTML = card;
-	}
-
-	test();
-
-
-	
+		
 			
 	 }// end of employeeData.
-
 	 	
 
 	$.getJSON(randomUserAPI, randomUserOptions, employeeData);
