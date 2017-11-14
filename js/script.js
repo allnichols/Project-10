c0b2172c1efb0104fdc497f56401995c4a69dc3c
Max Nichols <allenmaxnichols@gmail.com>
2017-11-13 20:04:16 -0600

    js changes

--- js/script.js
+++ js/script.js
@@ -1,57 +1,57 @@
 $(document).ready(function(){
+	const employeesContainer = document.getElementById("wrapper");
+		
 	
 	
-	var randomUserAPI = 'https://randomuser.me/api/?results=12&nat=us';
+	
+					// closing the modal
+					var close = document.getElementsByClassName("close")[0];
+
+					close.addEventListener("click", () => content.style.display = "none");
+
+					 function employeeData(results){
+					 		//getting employess data
+
+					 		let employeeDetails = results.results;										
+								for (var i = 0; i < employeeDetails.length ; i++) {
+									let member = employeeDetails[i];
+									let picture = member.picture.large;
+									let image = '<img src="'+ picture +'">';
+									let fullName = member.name.first + " " + member.name.last;
+									let email = member.email;
+									let cell = member.cell;
+									let street = member.location.street;
+									let city = member.location.city;
+									let state = member.location.state;
+									let postcode = member.location.postcode;
+
+
+									employeeInfo.push({
+								"image": picture,
+								"name": fullName,
+								"email" : email,
+								"cell" : cell,
+								"street" : street,
+								"city" : city,
+								"state" : state,
+								"postcode" : postcode
+							});
+					
+								}
+					 }// end of employeeData.
+
+	const employeeInfo = [];
+	console.log(employeeInfo);
+	console.log(employeeInfo[0]);
+
+
+
+	//AJAX
+	var randomUserAPI = 'https://randomuser.me/api/?results=12&inc=picture,name,email,location,cell,dob,nat&nat=us';
 	var randomUserOptions = {
 		dataType: 'json'
-	};//end AJAX
-
-	
-
-	 function employeeData(data){
-	 	//getting employee info
-		
-		let employeeDetails = data.results;
-		let cardHTML = '<section>';
-		
-		for(let i=0; i< employeeDetails.length; i++){
-			let member = employeeDetails[i];
-			cardHTML += '<div class="member" id="overlay">';
-			cardHTML += '<img src="' + member.picture.large + '">';
-			cardHTML += '<h2>' + member.name.first + " " + member.name.last + '</h2>';
-			cardHTML += '<p>' + member.email + '</p>';
-			cardHTML += '<p>' + member.location.city + '</p>';
-			cardHTML += '</div>';
-			
-
-		};
-		cardHTML += '</section>';
-		document.getElementById("wrapper").innerHTML = cardHTML;
-
-		var overlay = document.getElementById("overlay");
-		var modal = document.getElementsByClassName("modal")[0];
-		var close = document.getElementsByClassName("close")[0];
-		var content = document.getElementsByClassName("modal-content")[0];
-		var info = overlay.innerHTML;
-		console.log(overlay.innerHTML)
-		
-		overlay.addEventListener("click", function(){
-			modal.style.display  = "block";
-			content.innerHTML += info;
-			
-		});
-
-		close.addEventListener("click", () => modal.style.display = "none");
-
-
-		// for (var i = 0; i < employeeDetails.length; i++) {
-		// 	console.log(employeeDetails[i]);
-		// }
-
-		
-			
-	 }// end of employeeData.
-	 	
+	};
+	//end AJAX
 
 	$.getJSON(randomUserAPI, randomUserOptions, employeeData);
 });
