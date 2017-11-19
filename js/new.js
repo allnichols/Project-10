0278c1940cefcfc53ad1d29720073de6276b579c
Max Nichols <allenmaxnichols@gmail.com>
2017-11-19 16:43:11 -0600

    js changes

--- js/new.js
+++ js/new.js
@@ -2,56 +2,93 @@ $(document).ready(() =>{
 
 
 //AJAX variables
-var randomUserAPI = 'https://randomuser.me/api/?results=12&nat=us,gb&inc=name,email,location,picture,username,cell,dob'
+var randomUserAPI = 'https://randomuser.me/api/?results=12&nat=us&inc=name,email,location,picture,username,cell,dob'
 var options = {
 	datatype: 'json'
 }
 // end of Ajax Variables
-
-					var card 		= document.getElementsByClassName("member");
+					
+					var cardWrapper = document.getElementById("wrapper");
 					var overlay 	= document.getElementById('modal');					
 					var close 		= document.getElementsByClassName('close')[0];
-					var modalCard   = document.getElementsByClassName('modalContent');
+					var modal 		= document.getElementById('modal-content');
+					var info		= [];
+					var memberTotal = 12;
+					console.log(info);
+				
+					
+					
 
-					function memberData(data){
-						let member = data.results;
-						for (var i = 0; i < member.length; i++) {
-							 // console.log(member[i].email);
-							 
-							 memberCard(member[i]);
-							
-						}	
+					function getMemberData(data){
+							let member = data.results
+						for (var i = 0; i < member.length; i++) {	
+								let person  = member[i];
+								let name    = person.name.first + " " + person.name.last;
+								let image   = person.picture.large;
+								let email   = person.email;
+								let city    = person.location.city;
+								let address = person.location.street + " " + ", " +person.location.state + " " + person.location.postcode;
+								
+								memberCard(person);
+								//modalContent(person);
+								// console.log(address);
+
+								info.push({
+									"index" 	: i,
+									"img"		: image,
+									"name"		: name,
+									"email" 	: email,
+									"city"		: city,
+									"address" 	: address
+								})
+															 
+						}													
 					}
-
-					//creates the members card
+					
+						//creates the members card
 					function memberCard(data){
+
 						let cardHtml = '<div class="member">';
-						cardHtml += '<img src="' + data.picture.large + '">';
-						cardHtml += '<p>' + data.name.first + " " + data.name.last + '</p>';
-						cardHtml += '<p>' + data.email + '</p>';
-						cardHtml += '<p>' + data.location.city + '</p>';
-						cardHtml += '</div>';
+							cardHtml += '<img src="' + data.picture.large + '">';
+							cardHtml += '<p>' + data.name.first + " " + data.name.last + '</p>';
+							cardHtml += '<p>' + data.email + '</p>';
+							cardHtml += '<p>' + data.location.city + '</p>';
+							cardHtml += '</div>';
 
-						//adding a click event to each member card.
-						for (var i = 0; i < card.length; i++) {
-							card[i].addEventListener("click", function(){
-								overlay.style.display = "block";
-						});
-						};
 
-							$("#wrapper").append(cardHtml);
-
-							
-
+						$("#wrapper").append(cardHtml);								
 					}
 					//end of card creation
 
+				
+
+					function modalContent(data){
+							let cardHtml = '<div class="member">';
+							cardHtml += '<img src="' + data.picture.large + '">';
+							cardHtml += '<p>' + data.name.first + " " + data.name.last + '</p>';
+							cardHtml += '<p>' + data.email + '</p>';
+							cardHtml += '<p>' + data.location.city + '</p>';
+							cardHtml += '</div>';
 		
+							$("#modal-content").append(cardHtml);
+					}	
+					//opening the modal 
+
+				cardWrapper.addEventListener('click', (e) =>{
+					if(e.target.className == 'member') {
+						let target = e.target;
+						let targetID = $(target).html();
+						//modalContent(info[targetID]);
+						console.log(targetID);
+					} 
+
+				})
 
 					
-
-
-
+								
+				
+					
+														
 
 					//closing the modal overlay.
 					close.addEventListener("click", ()=> overlay.style.display = "none");
@@ -59,6 +96,6 @@ var options = {
 
 			
 
-$.getJSON(randomUserAPI, options, memberData);
+$.getJSON(randomUserAPI, options, getMemberData);
 });
 
